1777dc4e2025878f70e5d5156a6a6b42573e160e
«Uncle_Bee» <afrol95300@gmail.com>
2025-12-06 22:36:36 +0400

    Fix: use GourmetHubApp constructor in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:gourmet_hub/providers/cart_provider.dart';
 import 'main_page.dart';
 import 'theme/app_theme.dart';
 void main() {
-  runApp(const GourmetHubApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => CartProvider(),
+      child: const GourmetHubApp(),   // ← оставь своё название приложения точно как было!
+    ),
+  );
 }
 
 class GourmetHubApp extends StatelessWidget {
